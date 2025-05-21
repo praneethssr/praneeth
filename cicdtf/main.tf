b4923801aaac7c1b2335ef94a5d2f65daca65c6b
SANAGAPALLISRI RAM PRANEETH <100183975+praneethssr@users.noreply.github.com>
2025-05-21 10:51:51 +0530

    Update main.tf

--- cicdtf/main.tf
+++ cicdtf/main.tf
@@ -18,3 +18,10 @@ module "ec2" {
   vpc_id        = module.vpc.vpc_id
   subnet_id     = module.vpc.public_subnet_id
 }
+# cicdtf/main.tf (Corrected)
+
+resource "aws_key_pair" "deployer_key" {
+  public_key = var.public_key # <--- Use the variable here
+  key_name   = "deployer-key" # Ensure you have a key_name defined, e.g., "deployer-key"
+  # ... other attributes
+}
