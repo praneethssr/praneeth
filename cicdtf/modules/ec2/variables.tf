dd623743cc5bd0e68c7d8181054b051e041bd608
SANAGAPALLISRI RAM PRANEETH <100183975+praneethssr@users.noreply.github.com>
2025-05-22 12:49:25 +0530

    Update variables.tf

--- cicdtf/modules/ec2/variables.tf
+++ cicdtf/modules/ec2/variables.tf
@@ -1,26 +1,37 @@
 variable "ami" {
-  type    = string
-  default = ""
+  type        = string
+  default     = ""
+  description = "AMI ID (optional). If empty, Amazon Linux 2 will be used."
 }
 
 variable "instance_type" {
-  type = string
+  type        = string
+  description = "EC2 instance type"
 }
 
 variable "key_name" {
-  type = string
-  description = "Name of the EC2 key pair to use"
+  type        = string
+  default     = ""
+  description = "Optional: existing EC2 key pair name"
 }
 
+variable "public_key" {
+  type        = string
+  default     = ""
+  description = "Optional: public key content to create a new EC2 key pair"
+}
 
 variable "instance_name" {
-  type = string
+  type        = string
+  description = "Tag name for the EC2 instance"
 }
 
 variable "vpc_id" {
-  type = string
+  type        = string
+  description = "VPC ID"
 }
 
 variable "subnet_id" {
-  type = string
+  type        = string
+  description = "Subnet ID"
 }
