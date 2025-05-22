0d2c72d5016058383317911afbb409fba9f094d3
SANAGAPALLISRI RAM PRANEETH <100183975+praneethssr@users.noreply.github.com>
2025-05-22 19:40:48 +0530

    Update main.tf

--- cicdtf/modules/vpc/main.tf
+++ cicdtf/modules/vpc/main.tf
@@ -1,31 +1,5 @@
 # modules/vpc/main.tf
 
-# Input variables for the VPC module
-variable "cidr_block" {
-  description = "The CIDR block for the VPC."
-  type        = string
-}
-
-variable "vpc_name" {
-  description = "The name tag for the VPC."
-  type        = string
-}
-
-variable "subnet_cidr" {
-  description = "The CIDR block for the public subnet."
-  type        = string
-}
-
-variable "az" {
-  description = "The Availability Zone for the public subnet."
-  type        = string
-}
-
-variable "subnet_name" {
-  description = "The name tag for the public subnet."
-  type        = string
-}
-
 # Resource: AWS VPC
 # Creates the main Virtual Private Cloud.
 resource "aws_vpc" "main" {
