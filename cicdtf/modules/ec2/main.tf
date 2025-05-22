d7b6c8280c43d01047ce74eb594c768b6773a3ed
SANAGAPALLISRI RAM PRANEETH <100183975+praneethssr@users.noreply.github.com>
2025-05-22 19:01:00 +0530

    Update main.tf

--- cicdtf/modules/ec2/main.tf
+++ cicdtf/modules/ec2/main.tf
@@ -1,42 +1,29 @@
-data "aws_ami" "amazon_linux" {
-  most_recent = true
-  owners      = ["amazon"]
-
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
-  }
+variable "ami" {
+  description = "AMI ID to use for the instance"
+  type        = string
 }
 
-resource "aws_security_group" "ec2_sg" {
-  name        = "ec2-security-group"
-  description = "Allow SSH"
-  vpc_id      = var.vpc_id
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
 }
 
-resource "aws_instance" "this" {
-  ami                         = var.ami != "" ? var.ami : data.aws_ami.amazon_linux.id
-  instance_type               = var.instance_type
-  key_name                    = var.key_name
-  subnet_id                   = var.subnet_id
-  vpc_security_group_ids      = [aws_security_group.ec2_sg.id]
-
-  tags = {
-    Name = var.instance_name
-  }
+variable "key_name" {
+  description = "Key pair name for SSH access"
+  type        = string
 }
 
+variable "instance_name" {
+  description = "Name tag for the instance"
+  type        = string
+}
+
+variable "vpc_id" {
+  description = "VPC ID"
+  type        = string
+}
+
+variable "subnet_id" {
+  description = "Subnet ID"
+  type        = string
+}
