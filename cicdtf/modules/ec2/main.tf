5090dd73fadeff91379166a638ee2d80f9ec5bd6
SANAGAPALLISRI RAM PRANEETH <100183975+praneethssr@users.noreply.github.com>
2025-05-22 19:38:54 +0530

    Update main.tf

--- cicdtf/modules/ec2/main.tf
+++ cicdtf/modules/ec2/main.tf
@@ -1,41 +1,5 @@
 # modules/ec2/main.tf
 
-# Input variables for the EC2 module
-variable "ami" {
-  description = "The AMI ID for the EC2 instance."
-  type        = string
-}
-
-variable "instance_type" {
-  description = "The instance type for the EC2 instance."
-  type        = string
-}
-
-variable "key_name" {
-  description = "The name of the SSH key pair to use for the EC2 instance."
-  type        = string
-}
-
-variable "instance_name" {
-  description = "The name tag for the EC2 instance."
-  type        = string
-}
-
-variable "vpc_id" {
-  description = "The ID of the VPC where the EC2 instance will be launched."
-  type        = string
-}
-
-variable "subnet_id" {
-  description = "The ID of the subnet where the EC2 instance will be launched."
-  type        = string
-}
-
-variable "public_key" {
-  description = "The SSH public key content for the EC2 instance (for security group rules)."
-  type        = string
-}
-
 # Resource: Security Group for EC2
 # Creates a security group that allows inbound SSH (port 22) access from anywhere.
 resource "aws_security_group" "ec2_sg" {
