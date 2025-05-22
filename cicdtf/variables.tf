f9af6a5a0bda1aa9cf5e1f36a6b690574e06e3c5
SANAGAPALLISRI RAM PRANEETH <100183975+praneethssr@users.noreply.github.com>
2025-05-22 15:19:18 +0530

    Update variables.tf

--- cicdtf/variables.tf
+++ cicdtf/variables.tf
@@ -1,16 +1,10 @@
 variable "public_key" {
-  description = "The SSH public key content for the EC2 Key Pair."
+  description = "Public key content for EC2 key pair"
   type        = string
 }
 
 variable "instance_az" {
-  description = "The AWS Availability Zone for the EC2 instance and public subnet."
+  description = "Availability zone for subnet"
   type        = string
   default     = "ap-south-1a"
-}
-
-variable "aws_region" {
-  description = "The AWS region to deploy resources into."
-  type        = string
-  default     = "ap-south-1"
 }
