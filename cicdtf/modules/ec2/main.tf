08a9111461eac723dd317b0317f36ce41c8fa03a
SANAGAPALLISRI RAM PRANEETH <100183975+praneethssr@users.noreply.github.com>
2025-05-22 19:01:26 +0530

    Update main.tf

--- cicdtf/modules/ec2/main.tf
+++ cicdtf/modules/ec2/main.tf
@@ -1,29 +1,11 @@
-variable "ami" {
-  description = "AMI ID to use for the instance"
-  type        = string
-}
+resource "aws_instance" "this" {
+  ami                         = var.ami
+  instance_type               = var.instance_type
+  key_name                    = var.key_name
+  subnet_id                   = var.subnet_id
+  associate_public_ip_address = true
 
-variable "instance_type" {
-  description = "EC2 instance type"
-  type        = string
-}
-
-variable "key_name" {
-  description = "Key pair name for SSH access"
-  type        = string
-}
-
-variable "instance_name" {
-  description = "Name tag for the instance"
-  type        = string
-}
-
-variable "vpc_id" {
-  description = "VPC ID"
-  type        = string
-}
-
-variable "subnet_id" {
-  description = "Subnet ID"
-  type        = string
+  tags = {
+    Name = var.instance_name
+  }
 }
