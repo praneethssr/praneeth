c0050f4d57fe95efcf4e18ca653443581bc3170f
SANAGAPALLISRI RAM PRANEETH <100183975+praneethssr@users.noreply.github.com>
2025-05-22 19:35:17 +0530

    Update outputs.tf

--- cicdtf/modules/ec2/outputs.tf
+++ cicdtf/modules/ec2/outputs.tf
@@ -1,14 +1,22 @@
+# modules/ec2/outputs.tf
+
+# Output: Instance ID
+# Exports the ID of the created EC2 instance.
 output "instance_id" {
-  description = "The ID of the EC2 instance"
+  description = "The ID of the EC2 instance."
   value       = aws_instance.ec2_instance.id
 }
 
+# Output: Instance Public IP
+# Exports the public IP address of the EC2 instance.
 output "instance_public_ip" {
-  description = "The public IP address of the EC2 instance"
+  description = "The public IP address of the EC2 instance."
   value       = aws_instance.ec2_instance.public_ip
 }
 
+# Output: Instance Private IP
+# Exports the private IP address of the EC2 instance.
 output "instance_private_ip" {
-  description = "The private IP address of the EC2 instance"
+  description = "The private IP address of the EC2 instance."
   value       = aws_instance.ec2_instance.private_ip
 }
