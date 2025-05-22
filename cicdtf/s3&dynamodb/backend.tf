abe564a2e0b0b6faec8cbdb4a2f2efe729ae673f
pr5640 <praneethssr.2002@gmail.com>
2025-05-22 18:31:45 +0530

    all the floder

--- cicdtf/s3&dynamodb/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "terraform-state-ssr123-ap-south-1"
-    key            = "terraform.tfstate"
-    region         = "ap-south-1"
-    dynamodb_table = "db-state-lock"
-    encrypt        = true
-  }
-}
