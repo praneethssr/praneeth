9b093736b08cde4376d5bb13987f4c77dad8771c
SANAGAPALLISRI RAM PRANEETH <100183975+praneethssr@users.noreply.github.com>
2025-05-22 18:56:12 +0530

    Update variables.tf

--- cicdtf/modules/vpc/variables.tf
+++ cicdtf/modules/vpc/variables.tf
@@ -1,49 +1,55 @@
+# VPC and subnet variables
 variable "cidr_block" {
+  description = "CIDR block for the VPC"
   type        = string
   default     = "10.0.0.0/16"
 }
 
-variable "subnet_cidr" {
-  type        = string
-  default     = "10.0.1.0/24"
-}
-
 variable "vpc_name" {
+  description = "Name of the VPC"
   type        = string
   default     = "my-vpc"
 }
 
+variable "subnet_cidr" {
+  description = "CIDR block for the subnet"
+  type        = string
+  default     = "10.0.1.0/24"
+}
+
 variable "subnet_name" {
+  description = "Name of the subnet"
   type        = string
   default     = "my-subnet"
 }
 
 variable "az" {
+  description = "Availability zone for subnet"
   type        = string
   default     = "ap-south-1a"
 }
 
+# EC2 instance variables
 variable "ami" {
+  description = "AMI ID to use for the EC2 instance"
   type        = string
-  default     = "ami-0c55b159cbfafe1f0"
+  default     = "ami-0c55b159cbfafe1f0" # Change this to a valid AMI if needed
 }
 
 variable "instance_type" {
+  description = "EC2 instance type"
   type        = string
   default     = "t2.micro"
 }
 
 variable "key_name" {
+  description = "Key pair name for SSH access"
   type        = string
-  default     = "-key"
+  default     = "my-deployer-key"  # Change if you want
 }
 
 variable "instance_name" {
+  description = "Name tag for the EC2 instance"
   type        = string
-  default     = "my-ec2"
-}
-variable "vpc_cidr" {
-  description = "CIDR block for the VPC"
-  type        = string
-  default     = "10.0.0.0/16"  # or whatever CIDR you want
+  default     = "my-ec2-instance"
 }
