63941dc0409cd3d99b2d67a271462e69c09ed4cc
SANAGAPALLISRI RAM PRANEETH <100183975+praneethssr@users.noreply.github.com>
2025-05-22 19:36:57 +0530

    Update main.tf

--- cicdtf/modules/ec2/main.tf
+++ cicdtf/modules/ec2/main.tf
@@ -1,15 +1,78 @@
-resource "aws_instance" "this" {
-  ami                         = var.ami
-  instance_type               = var.instance_type
-  key_name                    = var.key_name
-  subnet_id                   = var.subnet_id
-  associate_public_ip_address = true
+# modules/ec2/main.tf
+
+# Input variables for the EC2 module
+variable "ami" {
+  description = "The AMI ID for the EC2 instance."
+  type        = string
+}
+
+variable "instance_type" {
+  description = "The instance type for the EC2 instance."
+  type        = string
+}
+
+variable "key_name" {
+  description = "The name of the SSH key pair to use for the EC2 instance."
+  type        = string
+}
+
+variable "instance_name" {
+  description = "The name tag for the EC2 instance."
+  type        = string
+}
+
+variable "vpc_id" {
+  description = "The ID of the VPC where the EC2 instance will be launched."
+  type        = string
+}
+
+variable "subnet_id" {
+  description = "The ID of the subnet where the EC2 instance will be launched."
+  type        = string
+}
+
+variable "public_key" {
+  description = "The SSH public key content for the EC2 instance (for security group rules)."
+  type        = string
+}
+
+# Resource: Security Group for EC2
+# Creates a security group that allows inbound SSH (port 22) access from anywhere.
+resource "aws_security_group" "ec2_sg" {
+  name_prefix = "${var.instance_name}-sg-"
+  description = "Allow SSH inbound traffic"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description = "SSH from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # WARNING: For production, restrict this to known IPs
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1" # All protocols
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.instance_name}-sg"
+  }
+}
+
+# Resource: EC2 Instance
+# Provisions the EC2 instance.
+resource "aws_instance" "ec2_instance" {
+  ami           = var.ami
+  instance_type = var.instance_type
+  key_name      = var.key_name
+  subnet_id     = var.subnet_id
+  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
 
   tags = {
     Name = var.instance_name
   }
-}
-resource "aws_key_pair" "deployer_key" {
-  key_name   = var.key_name
-  public_key = var.ssh_public_key
 }
