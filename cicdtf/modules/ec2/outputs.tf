4ac05ac56dbed1f3c3fea44c70ccfb3c55e6859d
SANAGAPALLISRI RAM PRANEETH <100183975+praneethssr@users.noreply.github.com>
2025-05-22 19:14:52 +0530

    Update outputs.tf

--- cicdtf/modules/ec2/outputs.tf
+++ cicdtf/modules/ec2/outputs.tf
@@ -1,6 +1,7 @@
-output "security_group_id" {
-  value = aws_security_group.ec2_sg.id
+output "vpc_id" {
+  value = aws_vpc.main.id
 }
-output "instance_id" {
-  value = aws_instance.this.id
+
+output "public_subnet_id" {
+  value = aws_subnet.public.id
 }
