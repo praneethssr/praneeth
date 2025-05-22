7e62fa27485db5add96312f1311ae0c358ced86f
SANAGAPALLISRI RAM PRANEETH <100183975+praneethssr@users.noreply.github.com>
2025-05-22 19:15:55 +0530

    Update outputs.tf

--- cicdtf/modules/vpc/outputs.tf
+++ cicdtf/modules/vpc/outputs.tf
@@ -1,13 +1,7 @@
-# modules/vpc/outputs.tf
-
 output "vpc_id" {
-  description = "The ID of the VPC"
-  value       = aws_vpc.main.id
+  value = aws_vpc.main.id
 }
 
 output "public_subnet_id" {
-  description = "The ID of the main public subnet"
-  value       = aws_subnet.main.id # <--- CHANGED FROM aws_subnet.public.id
+  value = aws_subnet.public.id
 }
-
-# Add other outputs if your VPC module has them, e.g., for private subnets, security group IDs, etc.
