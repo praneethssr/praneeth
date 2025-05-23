50cf5c7faa4db27d52d10469510e03ed1a2fa8c6
SANAGAPALLISRI RAM PRANEETH <100183975+praneethssr@users.noreply.github.com>
2025-05-23 11:48:19 +0530

    Update main.tf

--- cicdtf/modules/ec2/main.tf
+++ cicdtf/modules/ec2/main.tf
@@ -1,10 +1,13 @@
-resource "aws_security_group" "ec2_sg" {
-  name_prefix = "${var.instance_name}-sg-"
+# --------------------------
+# Security Group for EC2
+# --------------------------
+resource "aws_security_group" "instance_sg" {
+  name        = "${var.instance_name}-sg"
   description = "Allow SSH inbound traffic"
   vpc_id      = var.vpc_id
 
   ingress {
-    description = "SSH"
+    description = "SSH from anywhere"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
@@ -14,7 +17,7 @@ resource "aws_security_group" "ec2_sg" {
   egress {
     from_port   = 0
     to_port     = 0
-    protocol    = "-1"
+    protocol    = "-1" # All protocols
     cidr_blocks = ["0.0.0.0/0"]
   }
 
@@ -23,12 +26,15 @@ resource "aws_security_group" "ec2_sg" {
   }
 }
 
-resource "aws_instance" "ec2_instance" {
-  ami                    = var.ami
-  instance_type          = var.instance_type
-  subnet_id              = var.subnet_id
-  key_name               = var.key_name
-  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
+# --------------------------
+# EC2 Instance Resource
+# --------------------------
+resource "aws_instance" "web" {
+  ami           = var.ami
+  instance_type = var.instance_type
+  key_name      = var.key_name
+  subnet_id     = var.subnet_id
+  vpc_security_group_ids = [aws_security_group.instance_sg.id]
 
   tags = {
     Name = var.instance_name
