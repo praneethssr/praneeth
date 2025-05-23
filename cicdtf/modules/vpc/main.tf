14560e6373b74629a04370e0bb27449ba09e0867
SANAGAPALLISRI RAM PRANEETH <100183975+praneethssr@users.noreply.github.com>
2025-05-23 11:52:22 +0530

    Update main.tf

--- cicdtf/modules/vpc/main.tf
+++ cicdtf/modules/vpc/main.tf
@@ -1,18 +1,62 @@
+# --------------------------
+# VPC Resource
+# --------------------------
 resource "aws_vpc" "main" {
   cidr_block = var.cidr_block
+  enable_dns_hostnames = true
+  enable_dns_support   = true
 
   tags = {
     Name = var.vpc_name
   }
 }
 
+# --------------------------
+# Internet Gateway Resource
+# --------------------------
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.vpc_name}-igw"
+  }
+}
+
+# --------------------------
+# Public Subnet Resource
+# --------------------------
 resource "aws_subnet" "public" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = var.subnet_cidr
-  availability_zone       = var.az
-  map_public_ip_on_launch = true
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.subnet_cidr
+  availability_zone = var.az
+  map_public_ip_on_launch = true # Automatically assign public IPs
 
   tags = {
     Name = var.subnet_name
   }
 }
+
+# --------------------------
+# Route Table for Public Subnet
+# --------------------------
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0" # Allow all outbound traffic
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "${var.vpc_name}-public-rt"
+  }
+}
+
+# --------------------------
+# Route Table Association
+# --------------------------
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
+
