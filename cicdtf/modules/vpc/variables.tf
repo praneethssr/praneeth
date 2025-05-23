2b0358f5a1b6a5deab9c8871928c5db5509f94f0
SANAGAPALLISRI RAM PRANEETH <100183975+praneethssr@users.noreply.github.com>
2025-05-23 11:53:05 +0530

    Update variables.tf

--- cicdtf/modules/vpc/variables.tf
+++ cicdtf/modules/vpc/variables.tf
@@ -1,5 +1,24 @@
-variable "cidr_block" {}
-variable "vpc_name" {}
-variable "subnet_cidr" {}
-variable "az" {}
-variable "subnet_name" {}
+variable "cidr_block" {
+  description = "The CIDR block for the VPC."
+  type        = string
+}
+
+variable "vpc_name" {
+  description = "The name of the VPC."
+  type        = string
+}
+
+variable "subnet_cidr" {
+  description = "The CIDR block for the public subnet."
+  type        = string
+}
+
+variable "az" {
+  description = "The Availability Zone for the public subnet."
+  type        = string
+}
+
+variable "subnet_name" {
+  description = "The name of the public subnet."
+  type        = string
+}
