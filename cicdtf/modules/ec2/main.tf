77c1317e7c210e1fbfaeb6548a1e8f539ce781ae
SANAGAPALLISRI RAM PRANEETH <100183975+praneethssr@users.noreply.github.com>
2025-05-23 14:39:00 +0530

    Update main.tf

--- cicdtf/modules/ec2/main.tf
+++ cicdtf/modules/ec2/main.tf
@@ -1,20 +1,5 @@
+# modules/ec2/main.tf
 
-# modules/ec2/main.tf (or modules/ec2/outputs.tf if you have a separate file)
-
-# ... (rest of your aws_instance.web resource configuration above) ...
-
-# --------------------------
-# EC2 Outputs
-# --------------------------
-output "instance_public_ip" {
-  description = "The public IP address of the EC2 instance."
-  value       = aws_instance.web.public_ip  # <<< CORRECTED: Reference aws_instance.web
-}
-
-output "instance_private_ip" {
-  description = "The private IP address of the EC2 instance."
-  value       = aws_instance.web.private_ip # <<< CORRECTED: Reference aws_instance.web
-=======
 # --------------------------
 # Security Group for EC2
 # --------------------------
@@ -56,10 +41,4 @@ resource "aws_instance" "web" {
   tags = {
     Name = var.instance_name
   }
-}
-
-# (If you had an "instance_id" output, correct it similarly)
-output "instance_id" {
-  description = "The ID of the EC2 instance."
-  value       = aws_instance.web.id       # <<< CORRECTED: Reference aws_instance.web
 }
