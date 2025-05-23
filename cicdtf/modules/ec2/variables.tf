78ad6a0a144c8cd027cdc8503a7805799f9b5dde
SANAGAPALLISRI RAM PRANEETH <100183975+praneethssr@users.noreply.github.com>
2025-05-23 11:48:59 +0530

    Update variables.tf

--- cicdtf/modules/ec2/variables.tf
+++ cicdtf/modules/ec2/variables.tf
@@ -1,29 +1,29 @@
 variable "ami" {
-  description = "AMI ID for the EC2 instance"
+  description = "The ID of the AMI to use for the EC2 instance."
   type        = string
 }
 
 variable "instance_type" {
-  description = "Type of the EC2 instance"
+  description = "The type of EC2 instance to launch."
   type        = string
 }
 
 variable "key_name" {
-  description = "Name of the AWS key pair to use for EC2"
+  description = "The name of the SSH key pair to use for the EC2 instance."
   type        = string
 }
 
 variable "instance_name" {
-  description = "Name tag for the EC2 instance"
+  description = "The name tag for the EC2 instance."
   type        = string
 }
 
 variable "vpc_id" {
-  description = "VPC ID where the EC2 will be deployed"
+  description = "The ID of the VPC where the EC2 instance will be launched."
   type        = string
 }
 
 variable "subnet_id" {
-  description = "Subnet ID for the EC2 instance"
+  description = "The ID of the subnet where the EC2 instance will be launched."
   type        = string
 }
