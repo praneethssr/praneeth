4dd7414fbbfc875ac0634b4ca92a1f50aeec4e6e
SANAGAPALLISRI RAM PRANEETH <100183975+praneethssr@users.noreply.github.com>
2025-05-23 14:39:19 +0530

    Update outputs.tf

--- cicdtf/modules/ec2/outputs.tf
+++ cicdtf/modules/ec2/outputs.tf
@@ -1,61 +1,16 @@
-# --------------------------
-# Security Group for EC2
-# --------------------------
-resource "aws_security_group" "instance_sg" {
-  name        = "${var.instance_name}-sg"
-  description = "Allow SSH inbound traffic"
-  vpc_id      = var.vpc_id
+# modules/ec2/outputs.tf
 
-  ingress {
-    description = "SSH from anywhere"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1" # All protocols
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "${var.instance_name}-sg"
-  }
-}
-
-# --------------------------
-# EC2 Instance Resource
-# This is the "aws_instance.web" that your outputs refer to.
-# --------------------------
-resource "aws_instance" "web" {
-  ami           = var.ami
-  instance_type = var.instance_type
-  key_name      = var.key_name
-  subnet_id     = var.subnet_id
-  vpc_security_group_ids = [aws_security_group.instance_sg.id]
-
-  tags = {
-    Name = var.instance_name
-  }
-}
-
-# --------------------------
-# EC2 Outputs
-# These outputs correctly reference the "aws_instance.web" resource.
-# --------------------------
-=======
-# EC2 Outputs
-# --------------------------
 output "instance_public_ip" {
-  description = "The public IP address of the EC2 instance"
+  description = "The public IP address of the EC2 instance."
   value       = aws_instance.web.public_ip
 }
 
 output "instance_private_ip" {
-  description = "The private IP address of the EC2 instance"
+  description = "The private IP address of the EC2 instance."
   value       = aws_instance.web.private_ip
 }
 
+output "instance_id" {
+  description = "The ID of the EC2 instance."
+  value       = aws_instance.web.id
+}
