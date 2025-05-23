af4fe7a0fbb81df03aa39661b3a4c31550b6f2f9
SANAGAPALLISRI RAM PRANEETH <100183975+praneethssr@users.noreply.github.com>
2025-05-23 11:52:47 +0530

    Update outputs.tf

--- cicdtf/modules/vpc/outputs.tf
+++ cicdtf/modules/vpc/outputs.tf
@@ -1,7 +1,13 @@
+
+# --------------------------
+# VPC Outputs
+# --------------------------
 output "vpc_id" {
-  value = aws_vpc.main.id
+  description = "The ID of the VPC"
+  value       = aws_vpc.main.id
 }
 
 output "public_subnet_id" {
-  value = aws_subnet.public.id
+  description = "The ID of the public subnet"
+  value       = aws_subnet.public.id
 }
