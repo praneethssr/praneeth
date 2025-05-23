17b6b1dcacf03786b624fec0da9fdfe5e09c4edb
pr5640 <praneethssr.2002@gmail.com>
2025-05-23 15:32:20 +0530

    Declared outputs in EC2 module

--- cicdtf/modules/ec2/outputs.tf
+++ cicdtf/modules/ec2/outputs.tf
@@ -1,5 +1,5 @@
-
 # modules/ec2/outputs.tf
+
 output "instance_public_ip" {
   description = "The public IP address of the EC2 instance."
   value       = aws_instance.web.public_ip
@@ -8,4 +8,9 @@ output "instance_public_ip" {
 output "instance_private_ip" {
   description = "The private IP address of the EC2 instance."
   value       = aws_instance.web.private_ip
+}
+
+output "instance_id" {
+  description = "The ID of the EC2 instance."
+  value       = aws_instance.web.id
 }
