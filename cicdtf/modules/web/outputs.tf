957f90a036ccb2792586e92d10944ed380403e8d
pr5640 <praneethssr.2002@gmail.com>
2025-05-23 15:59:31 +0530

    Updated web module placeholder files

---
+++ cicdtf/modules/web/outputs.tf
@@ -0,0 +1,2 @@
+# modules/web/outputs.tf
+# Declare any output variables for your web module here when you add resources.
---
+++ cicdtf/modules/web/variables.tf
@@ -0,0 +1,2 @@
+# modules/web/variables.tf
+# Declare any input variables for your web module here when you add resources.
