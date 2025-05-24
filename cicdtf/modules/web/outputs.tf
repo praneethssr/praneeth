395f80f05ec63e0770c7582b07f51b5802b29883
pr5640 <praneethssr.2002@gmail.com>
2025-05-24 09:28:02 +0530

    all file move into remote

--- cicdtf/modules/web/outputs.tf
+++ cicdtf/modules/web/outputs.tf
@@ -1,21 +1,4 @@
-# Corrected content for cicdtf/modules/web/outputs.tf
-
-# Output for the web instance ID
-output "web_instance_id" {
-  description = "The ID of the web server EC2 instance."
-  value       = aws_instance.web_instance.id
-}
-
-# Output for the public IP of the web instance (if applicable)
-output "web_public_ip" {
-  description = "The public IP address of the web server EC2 instance."
-  value       = aws_instance.web_instance.public_ip
-}
-
-# IMPORTANT: Any 'variable' declarations (like 'variable "instance_type"')
-# that were previously in this file have been removed.
-# Variables should only be declared in 'modules/web/variables.tf'.
-# cicdtf/modules/web/outputs.tf
+# cicdtf/modules/web/outputs.tf - CORRECTED to remove duplicate outputs
 
 output "web_instance_id" {
   description = "The ID of the web server EC2 instance."
