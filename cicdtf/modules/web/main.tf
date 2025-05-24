b508980282b689dc150b068d82ed238fe9906ba7
pr5640 <praneethssr.2002@gmail.com>
2025-05-24 09:25:41 +0530

    all file move into remote

--- cicdtf/modules/web/main.tf
+++ cicdtf/modules/web/main.tf
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-# FINAL Corrected content of cicdtf/modules/web/main.tf
-=======
-# cicdtf/modules/web/main.tf
->>>>>>> 1c3a492 (Update EC2, VPC, and Web modules with changes to main, outputs, and variables)
-
 resource "aws_instance" "web_instance" {
   ami           = var.ami_id
   instance_type = var.instance_type
@@ -13,9 +7,4 @@ resource "aws_instance" "web_instance" {
     Name = "${var.instance_name}-web"
   }
 }
-<<<<<<< HEAD
-=======
-
 # No 'some_nested_module' in this file.
-
->>>>>>> 1c3a492 (Update EC2, VPC, and Web modules with changes to main, outputs, and variables)
--- cicdtf/modules/web/outputs.tf
+++ cicdtf/modules/web/outputs.tf
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # Corrected content for cicdtf/modules/web/outputs.tf
 
 # Output for the web instance ID
@@ -16,7 +15,6 @@ output "web_public_ip" {
 # IMPORTANT: Any 'variable' declarations (like 'variable "instance_type"')
 # that were previously in this file have been removed.
 # Variables should only be declared in 'modules/web/variables.tf'.
-=======
 # cicdtf/modules/web/outputs.tf
 
 output "web_instance_id" {
@@ -28,4 +26,3 @@ output "web_public_ip" {
   description = "The public IP address of the web server EC2 instance."
   value       = aws_instance.web_instance.public_ip
 }
->>>>>>> 1c3a492 (Update EC2, VPC, and Web modules with changes to main, outputs, and variables)
