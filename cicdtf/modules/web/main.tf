dd985d483a7f08bc6e8697a56429b5de37888e41
pr5640 <praneethssr.2002@gmail.com>
2025-05-24 08:52:30 +0530

    all file move into remote

--- cicdtf/modules/web/main.tf
+++ cicdtf/modules/web/main.tf
@@ -1,9 +1,39 @@
-# Content of cicdtf/modules/web/main.tf
-resource "aws_instance" "web_server" {
-  # ... your web server configuration ...
-  # This is where the problematic module call might be!
+# Corrected content of cicdtf/modules/web/main.tf
+
+# Example web server instance definition
+resource "aws_instance" "web_instance" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  subnet_id     = var.subnet_id
+  key_name      = var.key_name
+  tags = {
+    Name = "${var.instance_name}-web"
+  }
+}
+
+# Corrected module call for "some_nested_module"
+# Assuming 'some_nested_module' is a sibling directory to 'web' in the 'modules' folder.
+# The path '../../some_nested_module_actual_directory' means:
+# 1. Go up one level from 'modules/web/' to 'modules/'
+# 2. Go up another level from 'modules/' to the project root 'cicdtf/'
+# 3. Go down into the 'modules/' directory
+# 4. Go into the 'some_nested_module_actual_directory' (replace with the actual name)
+module "some_nested_module" {
+  source = "../../some_nested_module_actual_directory" # <--- FIX THIS PATH
+  
+  # Add any required inputs for 'some_nested_module' here
   # For example:
-  # module "nested_module" {
-  #   source = "./modules/something" # <--- THIS IS THE KIND OF LINE CAUSING YOUR ERROR
-  # }
+  # input_variable_name = "some_value"
+}
+
+# Output for the web instance ID
+output "web_instance_id" {
+  description = "The ID of the web server EC2 instance."
+  value       = aws_instance.web_instance.id
+}
+
+# Output for the public IP of the web instance (if applicable)
+output "web_public_ip" {
+  description = "The public IP address of the web server EC2 instance."
+  value       = aws_instance.web_instance.public_ip
 }
--- cicdtf/modules/web/variables.tf
+++ cicdtf/modules/web/variables.tf
@@ -1,2 +1,26 @@
-# modules/web/variables.tf
-# Declare any input variables for your web module here when you add resources.
+# Content for cicdtf/modules/web/variables.tf
+
+variable "ami_id" {
+  description = "The ID of the AMI to use for the web instance."
+  type        = string
+}
+
+variable "instance_type" {
+  description = "The type of EC2 instance to launch for the web server."
+  type        = string
+}
+
+variable "subnet_id" {
+  description = "The ID of the subnet to deploy the web instance into."
+  type        = string
+}
+
+variable "key_name" {
+  description = "The name of the EC2 Key Pair to use for SSH access."
+  type        = string
+}
+
+variable "instance_name" {
+  description = "A name tag for the web EC2 instance."
+  type        = string
+}
